48655687d76d9ec5eca4cfae3affa2d400db5234
andreitotoro <andreitotoro@gmail.com>
2023-11-29 20:02:41 +0200

    Adaugat setari de baza ale paginii pentru a pastra tema

---
+++ components/Layout.tsx
@@ -0,0 +1,45 @@
+import { Box, Container, Stack, Text } from "@chakra-ui/react";
+import { Space_Mono } from "next/font/google";
+import React from "react";
+import Navbar from "./Navbar";
+import Main from "./Main";
+
+const spaceMonoFont = Space_Mono({
+  weight: ["400", "700"],
+  subsets: ["latin-ext"],
+});
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <Box
+      className={spaceMonoFont.className}
+      bgImage={"bg2.jpg"}
+      bgRepeat={"no-repeat"}
+      bgPos={"center"}
+      bgSize={["cover"]}
+    >
+      <Container
+        maxW={1500}
+        color={"white"}
+        px={[5, 5, 5, "2em"]}
+      >
+        <Stack minH={"100vh"}>
+          <Box
+            mt={["3em"]}
+            flex={1}
+          >
+            {children}
+          </Box>
+          <Text
+            fontSize={12}
+            py={"2em"}
+            justifySelf={"flex-end"}
+            color={"gray.400"}
+          >
+            ©️ 2023 - Colegiul Național Ion Creangă București
+          </Text>
+        </Stack>
+      </Container>
+    </Box>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,12 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { Box, Text } from "@chakra-ui/react";
+import Layout from "@/components/Layout";
 
 export default function Home() {
   return (
-    <Box>
-      <Text>This is a test of ChakraUI</Text>
-    </Box>
+    <Layout>
+      <Text>Setari Pagina</Text>
+    </Layout>
   );
 }
