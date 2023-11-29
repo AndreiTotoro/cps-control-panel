2cd6a681289fc5cd8b1a5593020baea2c515df01
andreitotoro <andreitotoro@gmail.com>
2023-11-29 23:08:27 +0200

    Change back to normal api routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,12 +21,12 @@ export default function Home() {
   const [linkVolumNou, setLinkVolumNou] = useState<string>("");
 
   const obtineVolume = async () => {
-    const volume = await axios.get<Volum[]>("./helpers/obtine-volume");
+    const volume = await axios.get<Volum[]>("./api/obtine-volume");
     setVolume(volume.data);
   };
 
   const adaugaVolum = async () => {
-    const volum = await axios.post<Volum>("./helpers/adauga-volum", {
+    const volum = await axios.post<Volum>("./api/adauga-volum", {
       titlu: titluVolumNou,
       link: linkVolumNou,
     });
@@ -36,7 +36,7 @@ export default function Home() {
   };
 
   const stergeVolum = async (id: string) => {
-    const volum = await axios.delete<Volum>("./helpers/sterge-volum", {
+    const volum = await axios.delete<Volum>("./api/sterge-volum", {
       data: {
         id,
       },
