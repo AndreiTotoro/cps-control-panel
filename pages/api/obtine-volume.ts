295dae4998cb085a8021c0602677b7775445a607
andreitotoro <andreitotoro@gmail.com>
2023-11-29 22:46:32 +0200

    Add error handling

--- pages/api/obtine-volume.ts
+++ pages/api/obtine-volume.ts
@@ -1,15 +1,21 @@
-import { Volum } from "@/types";
+import { Volume } from "@/types";
 import { PrismaClient } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
+import Error from "next/error";
 
 const prisma = new PrismaClient();
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Volum[]>
+  res: NextApiResponse<Volume[]>
 ) {
-  const volume = await prisma.volume.findMany({});
+  try {
+    const volume: Volume[] = await prisma.volume.findMany({});
 
-  res.status(200).json(volume);
-  prisma.$disconnect();
+    res.status(200).json(volume);
+  } catch (error: any) {
+    res.status(500).json(error);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
